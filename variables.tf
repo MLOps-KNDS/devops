007244d20d6c12a7b6ce2f9a59c1f18a9f0975f5
Mateusz Nędzi <nedzimateusz@gmail.com>
2023-03-26 20:35:38 +0200

    tf files added (#1)

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "PROJECT_ID" {
+  description = "A GCP project ID."
+  type        = string
+}
+
+variable "DEFAULT_REGION" {
+  description = "A default GCP region"
+  type        = string
+}
+
+variable "DEFAULT_ZONE" {
+  description = "A default GCP zone"
+  type        = string
+}
