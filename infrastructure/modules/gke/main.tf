82ec53f5119700c7260d3da56c3f38ed9fff0844
matined <nedzimateusz@gmail.com>
2023-06-25 22:07:39 +0200

    changes

--- infrastructure/modules/gke/main.tf
+++ infrastructure/modules/gke/main.tf
@@ -42,3 +42,16 @@ resource "google_compute_firewall" "rules" {
   }
   source_ranges = ["35.235.240.0/20"]
 }
+
+resource "google_compute_router" "router" {
+  name    = "nat-router"
+  network = var.network
+}
+
+module "cloud-nat" {
+  source                             = "terraform-google-modules/cloud-nat/google"
+  version                            = "~> 4.0"
+  router                             = google_compute_router.router.name
+  name                               = "nat-config"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
